a7715fcfa7b74ab2b36527caf0580dd34f847b68
Christian Geisler <christian.geisler@hogrefe.com>
2022-11-04 17:32:02 +0100

    vader: take text from post request

--- sentiment-analysis/serverless-vader/handler.py
+++ sentiment-analysis/serverless-vader/handler.py
@@ -1,17 +1,20 @@
 import json
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-def hello(event, context):
+def handle(event, context):
 
     analyzer = SentimentIntensityAnalyzer()
     sentence = "The plot was good, but the characters are uncompelling and the dialog is not great."
-    vs = analyzer.polarity_scores(sentence)
+
+    body = json.loads(event["body"])
+    text = body["text"]
+
+    vs = analyzer.polarity_scores(text)
 
     body = {
         "sentence": sentence,
         "score": str(vs),
-        "event": event,
-        "body": json.loads(event["body"])
+        "event": event
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
