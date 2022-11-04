46a2201f4d4ad7c1461a70920d750449b3fc8c39
Christian Geisler <christian.geisler@hogrefe.com>
2022-11-04 15:35:38 +0100

    added serverless hello world

---
+++ sentiment-analysis/serverless-vader/handler.py
@@ -0,0 +1,19 @@
+import json
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def hello(event, context):
+
+    analyzer = SentimentIntensityAnalyzer()
+    sentence = "The plot was good, but the characters are uncompelling and the dialog is not great."
+    vs = analyzer.polarity_scores(sentence)
+
+    body = {
+        "sentence": sentence,
+        "score": str(vs),
+        "event": event,
+        "context": context
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
